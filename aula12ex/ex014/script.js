272f4e322020f7c35daaf632ae60689ab4ddfe73
Daniel Ferreira Barbosa <125709368+DanielFBarbosa@users.noreply.github.com>
2023-03-25 01:44:37 -0300

    atualização projeto JS

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,21 +1,24 @@
-function carregar(): {
+function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    //var hora = data.getHours()
+    var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora >= 0 && hora < 12) {
-        //Bom Dia
-        img.src = 'fotomanha.png' alt = foto da manhã
-        else if(hora >= 12 && hora< 12 ) {
-            //Boa Tarde
-        img.src = 'fototarde.png' alt = 'foto da tarde'    
-            else {
-                //Boa Noite
-                img src = 'fotonoite.png' alt = 'Foto da noite'
-            }
-        }
+    if (hora >= 0 && hora <= 5) {
+        //' Boa Madrugada'
+       img.src = 'fotomadrugada.png'
+       document.body.style.background = '#fa7d20'
+    } else if (hora > 5 && hora <= 12) {
+        //'Bom Dia
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#bbb38a'
+    } else if (hora > 12 && hora <= 18) {
+       //'Boa Tarde'
+        img.src = 'fototarde.png'
+        document.body.style.background = '#fdc68b'
+    } else {
+        //'Boa Noite'
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#2b2c28'
     }
-}
-
- 
+} 
